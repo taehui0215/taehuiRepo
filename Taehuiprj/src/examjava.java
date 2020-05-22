d88a00afaf3dc0b6947ba22d60d9aa297c06d60c
DATA8320-18 <DATA8320-18@DESKTOP-G8IJA54>
2020-05-22 11:42:01 +0900

    과제

--- Taehuiprj/src/examjava.java
+++ Taehuiprj/src/examjava.java
@@ -1,14 +1,23 @@
-
 public class examjava {
-
 	public static void main(String[] args) {
-		int i;
-		i = 0;
+		int[][] aa = new int[3][4];
+		int i, k;
+		int val =1;
 		
-		while (i < 5) {
-			System.out.println("while 문을 공부합니다.\n");
-		i++;
-		}	
-
-	}
+	 for (i = 0; i < 3; i++) {
+		 for (k=0; k<4;k++) {
+			 aa[i][k] = val;
+			 val++;			 
+		 }
+	 }
+	 
+	 System.out.printf("aa[0][0]부터 aa[2][3]까지 출력 \n");
+	 
+	 for (i = 0; i < 3; i++) {
+		 for (k=0; k<4 ; k++) {
+			 System.out.printf("%3d ", aa[i][k]);
+		 }
+		 System.out.printf("\n");
+		 }
+	 }
 }
